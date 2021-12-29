b673c5b752149807be40a59331f36d667a6b1b0a
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2021-12-29 17:50:45 -0300

    Delete js-estudy directory

--- js-estudy/landscape.js
+++
@@ -1,7 +0,0 @@
-// implement the function if it's landscape or not (width > height)
-
-let sizes = isLandscape(1020,1080);
-console.log(sizes);
-function isLandscape(width, height){
-     return (width > height) ? "landscape" : "portrait";
-}
