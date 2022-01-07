83bfa9cd46889e7ec375e4f036ea1414772f6e31
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-01-06 21:04:56 -0300

    note good

--- 10Bigs/note/script.js
+++ 10Bigs/note/script.js
@@ -1,21 +1,33 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes){
+    notes.forEach(note => {
+        addNewNotes(note);
+    });
+}
+
 addBtn.addEventListener('click', () => {
     addNewNotes();
 });
 
-function addNewNotes(){
+function addNewNotes(text = ''){
     const note = document.createElement('div');
     note.classList.add('note');
 
     note.innerHTML = `
     <div class="notes">
         <div class="tools">
-            <button class="edit"><i class="fas fa-edit"></i></button>
-            <button class="delete"><i class="fas fa-trash"></i></button>
+            <button class="edit">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button class="delete">
+                <i class="fas fa-trash"></i>
+            </button>
         </div>
-        <div class="main hidden"></div>
-        <textarea></textarea>
+        <div class="main ${text? '' :'hidden'}"></div>
+        <textarea class="${text ?'hidden': "" }"></textarea>
     </div>`;
 
     const editBtn = note.querySelector('.edit');
@@ -23,6 +35,9 @@ function addNewNotes(){
 
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden');
@@ -37,8 +52,22 @@ function addNewNotes(){
         const {value} = e.target;
 
         main.innerHTML = marked(value);
+
+        updateLS();
     });
 
     document.body.appendChild(note);
+}
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
 
