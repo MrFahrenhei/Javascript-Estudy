83eac5f9374e9e3bab6990856ad87d1437ac9a2a
VVBeraldo <vvberaldo@minha.fag.edu.br>
2022-04-05 17:20:03 -0300

    oop

--- OOP JS/loja.js
+++ OOP JS/loja.js
@@ -1,19 +1,55 @@
-const btnClient = document.getElementById('btnClient');
-function addClient(){
+// const btnClient = document.getElementById('btnClient');
+// function addClient(){
 
-    let Cliente = {
-        // clientName: document.getElementById("nameClient"),
-        // clientTel:  document.getElementById("clientTel"),
-        // clientAdress: document.getElementById("clientAdress"),
-        // clientCPF: document.getElementById("clientCPF")
-        clientName: "John Doe",
-        clientTel:  "+5545999542380",
-        clientAdress: "Rua talz",
-        clientCPF: "85805050"
-    }
+//     let Cliente = {
+//         // clientName: document.getElementById("nameClient"),
+//         // clientTel:  document.getElementById("clientTel"),
+//         // clientAdress: document.getElementById("clientAdress"),
+//         // clientCPF: document.getElementById("clientCPF")
+//         clientName: "John Doe",
+//         clientTel:  "+5545999542380",
+//         clientAdress: "Rua talz",
+//         clientCPF: "85805050"
+//     }
 
+// }
+
+// btnClient.addEventListener('click', () => {
+//     addClient();
+// });
+
+class Cliente {
+    id;
+    nome;
+    cpf;
+    tel;
+    adress;
 }
 
-btnClient.addEventListener('click', () => {
-    addClient();
-});
+const cliente1 = new Cliente();
+
+cliente1.id = 1;
+cliente1.nome = "Joao";
+cliente1.cpf = 12334545654;
+cliente1.tel = 451123496843;
+cliente1.adress = "Rua talz";
+
+console.log(cliente1);
+
+class Produto{
+    id;
+    nome;
+    tipo;
+    modelo;
+    price;
+}
+
+const produto1 = new Produto();
+
+produto1.id = 1;
+produto1.nome = "Placa mãe";
+produto1.tipo = "motherboard";
+produto1.modelo = "Asus";
+produto1.price = 10562;
+
+console.log(produto1);
