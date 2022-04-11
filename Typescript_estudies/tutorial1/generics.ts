cb4955e9fc0e6a07f7421081d1b07b66d5679b4d
VVBeraldo <vvberaldo@minha.fag.edu.br>
2022-04-11 13:32:08 -0300

    arrumando o resto

---
+++ Typescript_estudies/tutorial1/generics.ts
@@ -0,0 +1,9 @@
+class Observable<T>{
+    constructor(public value: T){}
+}
+
+let x: Observable<number>;
+
+let y: Observable<Person>;
+
+// let 2 = new Observable(23);
---
+++ Typescript_estudies/tutorial1/js/generics.js
@@ -0,0 +1,9 @@
+class Observable {
+    value;
+    constructor(value) {
+        this.value = value;
+    }
+}
+let x;
+let y;
+// let 2 = new Observable(23);
