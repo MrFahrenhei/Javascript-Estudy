b23edcc0640b07eccbfccd18ea61d16f1251d6ca
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-07-07 19:55:56 -0300

    vazio

--- Typescript_estudies/typescript-curso-1-0596204f0dbaef2721bdce34df1a09888b809c8f/app/app.ts
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-import { Negociacao } from './controllers/negociacao.js';
-=======
-import { NegociacaoController } from "./controllers/necociacao-controller.js";
->>>>>>> 3ee948d (typescript)
-
-const controller = new NegociacaoController();
-const form = document.querySelector 
