9d67457fab363b6d526eb636acc4bcfb67399d9c
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-07-07 19:52:49 -0300

    typescript

---
+++ Typescript_estudies/tutorial2/index.ts
@@ -0,0 +1,9 @@
+function toFive(){
+    let i;
+    for(i = 0; i < 5; i++){
+        console.log(i);
+    }
+    console.log(`e por último ${i}`);
+}
+
+toFive();
--- Typescript_estudies/typescript-curso-1-0596204f0dbaef2721bdce34df1a09888b809c8f/app/app.ts
+++ Typescript_estudies/typescript-curso-1-0596204f0dbaef2721bdce34df1a09888b809c8f/app/app.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Negociacao } from './controllers/negociacao.js';
+=======
+import { NegociacaoController } from "./controllers/necociacao-controller.js";
+>>>>>>> 3ee948d (typescript)
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const form = document.querySelector 
